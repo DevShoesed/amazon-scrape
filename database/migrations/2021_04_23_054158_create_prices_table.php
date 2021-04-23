2736dfcc8eef2e03ce421fc9b868fcff1fc7e8a2
DevShoesed <francesco.scarpato@gmail.com>
2021-04-23 07:55:04 +0200

    Initial Commit with model and migration

---
+++ database/migrations/2021_04_23_054158_create_prices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->id();
+            $table->float('price');
+            $table->string('asin', 10);
+            $table->foreign('asin')->references('asin')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
