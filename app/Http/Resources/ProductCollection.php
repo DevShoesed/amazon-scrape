88ba125d53fb1ad030d0ed234dde9c276e5c13b0
DevShoesed <francesco.scarpato@gmail.com>
2021-04-24 08:52:25 +0200

    refactor: add count in index of product

--- app/Http/Resources/ProductCollection.php
+++ app/Http/Resources/ProductCollection.php
@@ -14,6 +14,9 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'data' => parent::toArray($request),
+            'count' => count(parent::toArray($request))
+        ];
     }
 }
